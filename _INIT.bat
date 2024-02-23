3c46e34320d42474d5bfe9c8aff0af5d576ca9a7
bemomemo <bemomemo8@gmail.com>
2024-02-23 21:52:18 +0100

    remove access token

--- _INIT.bat
+++ _INIT.bat
@@ -3,6 +3,6 @@ rmdir /s /q .git
 call __internal_update.bat
 
 git\cmd\git.exe init
-git\cmd\git.exe remote add origin https://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@github.com/bemomemo/easytdl
+git\cmd\git.exe remote add origin https://github.com/bemomemo/easytdl
 git\cmd\git.exe fetch
 git\cmd\git.exe reset --hard origin/master
