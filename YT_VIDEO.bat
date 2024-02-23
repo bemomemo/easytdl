52e856a3279e33ee3cb4976ef66da3e0c4a802c4
bemomemo <bemomemo8@gmail.com>
2024-02-23 21:43:20 +0100

    update permissions

--- YT_VIDEO.bat
+++ YT_VIDEO.bat
@@ -5,4 +5,7 @@ set /p url= input youtube url:
 "yt-dlp\yt-dlp.exe" %url% -F
 set /p format= input video + audio format (eg. 137+251): 
 "C:\Users\5800x\Desktop\yt-ffmpeg\yt-dlp.exe" %url% -f %format% --ffmpeg-location "ffmpeg\ffmpeg-2024-02-22-git-76b2bb96b4-full_build\bin\ffmpeg.exe" --external-downloader=aria2c --external-downloader-args "--min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16" -P output\Video
+
+start "" "output\Video"
+
 goto start
