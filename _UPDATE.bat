11cf9c8c717421b6516707447b6a6862bae106b7
bemomemo <bemomemo8@gmail.com>
2024-02-23 21:32:44 +0100

    v2 update

--- _UPDATE.bat
+++ _UPDATE.bat
@@ -1,3 +1,3 @@
-if exist "git\" git\cmd\git.exe pull
+if exist "git\" git\cmd\git.exe pull origin master
 
 call __internal_update.bat
--- __internal_update.bat
+++ __internal_update.bat
@@ -14,8 +14,6 @@ curl -L -o yt-dlp\yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/do
 curl -L -o git.zip https://github.com/git-for-windows/git/releases/download/v2.44.0-rc2.windows.1/MinGit-2.44.0-rc2-64-bit.zip
 curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/2024-02-22-git-76b2bb96b4/ffmpeg-2024-02-22-git-76b2bb96b4-full_build.zip
 
-pause
-
 echo "Extracting dependencies"
 
 tar -xf git.zip -C "git"
@@ -25,5 +23,3 @@ del git.zip
 del ffmpeg.zip
 
 echo "Done"
-
-pause
