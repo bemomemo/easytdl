b174233abf78efe59d1ca22fc8b32d719bde1bfe
bemomemo <bemomemo8@gmail.com>
2024-02-24 12:38:43 +0100

    fix urls with extra parameters

--- YT_AUDIO.bat
+++ YT_AUDIO.bat
@@ -2,7 +2,8 @@ if not exist "output\" mkdir output
 
 :start
 set /p url= input youtube url: 
-"yt-dlp\yt-dlp.exe" %url% -f 251 --extract-audio --audio-format mp3 --audio-quality 0 --ffmpeg-location "ffmpeg\ffmpeg-2024-02-22-git-76b2bb96b4-full_build\bin\ffmpeg.exe" --external-downloader=aria2c --external-downloader-args "--min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16" -P output\Audio
+FOR /F "delims=& tokens=1" %%A IN ("%url%") DO SET "newUrl=%%A"
+"yt-dlp\yt-dlp.exe" %newUrl% -f 251 --extract-audio --audio-format mp3 --audio-quality 0 --ffmpeg-location "ffmpeg\ffmpeg-2024-02-22-git-76b2bb96b4-full_build\bin\ffmpeg.exe" --external-downloader=aria2c --external-downloader-args "--min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16" -P output\Audio
 
 start "" "output\Audio"
 
--- YT_VIDEO.bat
+++ YT_VIDEO.bat
@@ -2,9 +2,10 @@ if not exist "output\" mkdir output
 
 :start
 set /p url= input youtube url: 
-"yt-dlp\yt-dlp.exe" %url% -F
+FOR /F "delims=& tokens=1" %%A IN ("%url%") DO SET "newUrl=%%A"
+"yt-dlp\yt-dlp.exe" %newUrl% -F
 set /p format= input video + audio format (eg. 137+251): 
-"C:\Users\5800x\Desktop\yt-ffmpeg\yt-dlp.exe" %url% -f %format% --ffmpeg-location "ffmpeg\ffmpeg-2024-02-22-git-76b2bb96b4-full_build\bin\ffmpeg.exe" --external-downloader=aria2c --external-downloader-args "--min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16" -P output\Video
+"C:\Users\5800x\Desktop\yt-ffmpeg\yt-dlp.exe" %newUrl% -f %format% --ffmpeg-location "ffmpeg\ffmpeg-2024-02-22-git-76b2bb96b4-full_build\bin\ffmpeg.exe" --external-downloader=aria2c --external-downloader-args "--min-split-size=1M --max-connection-per-server=16 --max-concurrent-downloads=16 --split=16" -P output\Video
 
 start "" "output\Video"
 
