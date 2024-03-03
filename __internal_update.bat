96c40e5410cd42058b519e35713156c70bcf4d26
bemomemo <bemomemo8@gmail.com>
2024-03-03 13:01:30 +0100

    curl no revoke

--- __internal_update.bat
+++ __internal_update.bat
@@ -10,9 +10,9 @@ mkdir yt-dlp
 
 echo "Downloading dependencies"
 
-curl -L -o yt-dlp\yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
-curl -L -o git.zip https://github.com/git-for-windows/git/releases/download/v2.44.0-rc2.windows.1/MinGit-2.44.0-rc2-64-bit.zip
-curl -L -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/2024-02-22-git-76b2bb96b4/ffmpeg-2024-02-22-git-76b2bb96b4-full_build.zip
+curl -L --ssl-no-revoke -o yt-dlp\yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
+curl -L --ssl-no-revoke -o git.zip https://github.com/git-for-windows/git/releases/download/v2.44.0-rc2.windows.1/MinGit-2.44.0-rc2-64-bit.zip
+curl -L --ssl-no-revoke -o ffmpeg.zip https://github.com/GyanD/codexffmpeg/releases/download/2024-02-22-git-76b2bb96b4/ffmpeg-2024-02-22-git-76b2bb96b4-full_build.zip
 
 echo "Extracting dependencies"
 
